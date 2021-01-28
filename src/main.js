b05ac3312f1efc34e46c88997c9d6dd01656916b
yangfanwudi2020 <121773725@qq.com>
2021-01-28 18:29:46 +0800

    项目初始化t添加less文件,导入

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
+import './styles/index.less'
 
 createApp(App).use(store).use(router).mount('#app')
